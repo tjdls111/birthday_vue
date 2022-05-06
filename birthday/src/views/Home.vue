d292b27ded14962ed673f57b7da84ee7670db95f
tjdls111 <tjdls111@naver.com>
2022-05-06 20:53:38 +0900

    feat: 메인 페이지 이미지 추가

--- birthday/src/views/Home.vue
+++ birthday/src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>home</div>
+  <div class="container">
+    <img src="../assets/main.png" alt="" />
+  </div>
 </template>
 
 <script>
@@ -11,4 +13,7 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.container {
+}
+</style>
